67e79f8b1c81d26d971202a595746c709e3b46f5
maxx-zabelenkov <maxx.zabelenkov@gmail.com>
2025-01-23 13:37:36 +0200

    Js Added

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Happy developing ✨')
+async function shortURL() {
+    const longUrl = document.getElementById("url").value;
+    const response = await fetch(`https://tinyurl.com/api-create.php?url=${longUrl}`);
+    if (response.ok) {
+        const shortUrl = await response.text();
+        document.getElementById('result').innerHTML = `
+        Your shortend URL : <a class="shortURL" href="${shortUrl }" target="_blank">${shortUrl}</a>`;
+    }
+    else{
+        document.getElementById('result').innerHTML = "Error shortening"
+    }
+
+}
